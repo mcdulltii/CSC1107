dea68c1a2e257e4d8ee2f7f1a983a61ef303dc46
mcdulltii <mcdulltii@gmail.com>
2023-05-29 12:10:24 -0400

    Added table printing helper library

--- Qn2/Q2_Group_7.c
+++ Qn2/Q2_Group_7.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "attr.c"
+#include "fort.c"
 
 int main(int argc, char *argv[]) {
     int** proc_attr_table = gen_attr();
@@ -8,9 +9,27 @@ int main(int argc, char *argv[]) {
     int* burst_time = proc_attr_table[1];
     int* priority = proc_attr_table[2];
 
+    // Create process attribute table
+    ft_table_t *attr_table = ft_create_table();
+
+    // Setup header
+    ft_set_cell_prop(attr_table, 0, FT_ANY_COLUMN, FT_CPROP_ROW_TYPE, FT_ROW_HEADER);
+    ft_write_ln(attr_table, "Process", "Arrival Time", "Burst Time", "Priority");
+
+    // Build table values
+    char i_str[3], arrival_time_str[2], burst_time_str[3], priority_str[2];
     for (int i=0; i<NUM_PROC; i++) {
-        printf("Process %d: %d %d %d\n", i, arrival_time[i], burst_time[i], priority[i]);
+        sprintf(i_str, "P%d", i);
+        sprintf(arrival_time_str, "%d", arrival_time[i]);
+        sprintf(burst_time_str, "%d", burst_time[i]);
+        sprintf(priority_str, "%d", priority[i]);
+        ft_write_ln(attr_table, i_str, arrival_time_str, burst_time_str, priority_str);
     }
+
+    // Print table
+    printf("%s\n", ft_to_string(attr_table));
+    // Destroy table
+    ft_destroy_table(attr_table);
 
     free(arrival_time);
     free(burst_time);
