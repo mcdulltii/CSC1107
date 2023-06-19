f9f6299f96acf2b1f1efdd10bcc04b4c90e8a574
mcdulltii <mcdulltii@gmail.com>
2023-06-19 09:56:56 -0400

    Modified gantt chart visuals

--- Qn2/gantt.h
+++ Qn2/gantt.h
@@ -1,20 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 #include <malloc.h>
+#include "init.h"
 
 #ifndef GANTT_H
 #define GANTT_H
 
-void gantt(int* l) {
-    for (int c = 0; *l >= 0; c = !c, l++) {
-        if (!c) l[1] -= *l;
-        while (l[0]-- > 0) putchar(c? 45 : 32);
-        c? putchar(10) : 0;
-    }
-}
-
 void visualise_gantt(int* proc_sch_order, int* proc_sch_time, int array_len) {
-    // Draw gantt chart
-    gantt(proc_sch_time);
+#pragma region TIME_TABLE
+    // Create time table
+    ft_table_t *time_table = ft_create_table();
+
+    // Setup header
+    ft_set_cell_prop(time_table, 0, FT_ANY_COLUMN, FT_CPROP_ROW_TYPE, FT_ROW_HEADER);
+    ft_write_ln(time_table, "Gantt Chart");
+
+    // Iterate process scheduled time and print inline with process order
+    int prev_time = 0;
+    char gantt_str[500] = {};
+    char space_char[] = " ", dash_char[] = "-", newline_char[] = "\n";
+    for (int i=0; i<NUM_PROC; i++) {
+        prev_time = 0;
+        for (int j=0; j<array_len; j++) {
+            if (proc_sch_order[j] == i) {
+                int* gantt_time = &proc_sch_time[2 * j];
+                // Add spaces as gaps
+                for (int k=prev_time; k<gantt_time[0]; k++) strncat(gantt_str, space_char, 1);
+                // Add dashes as scheduled process
+                for (int k=gantt_time[0]; k<gantt_time[1]; k++) strncat(gantt_str, dash_char, 1);
+                // Save last index
+                prev_time = gantt_time[1];
+            }
+        }
+        strncat(gantt_str, newline_char, 1);
+    }
+    ft_write_ln(time_table, gantt_str);
 
 #pragma region GANTT_TABLE
     // Create gantt table
@@ -29,10 +49,17 @@ void visualise_gantt(int* proc_sch_order, int* proc_sch_time, int array_len) {
     }
 
     // Print table
-    printf("%s", ft_to_string(gantt_table));
+    ft_write_ln(time_table, ft_to_string(gantt_table));
+    ft_set_cell_prop(time_table, 2, 0, FT_CPROP_TEXT_ALIGN, FT_ALIGNED_CENTER);
+#pragma endregion GANTT_TABLE
+
+    // Print table
+    printf("%s", ft_to_string(time_table));
     // Destroy table
     ft_destroy_table(gantt_table);
-#pragma endregion GANTT_TABLE
+    // Destroy table
+    ft_destroy_table(time_table);
+#pragma endregion TIME_TABLE
 }
 
 #endif
